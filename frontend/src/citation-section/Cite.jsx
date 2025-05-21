ab9722b8c20c8031e1177e8ca784b6329a2d925e
Onur <onur.onel2230@gmail.com>
2025-05-21 19:46:55 -0400

    Updates

--- frontend/src/citation-section/Cite.jsx
+++ frontend/src/citation-section/Cite.jsx
@@ -1,11 +1,14 @@
 import Background from "../components/Background";
 import UrlManager from "./components/UrlManager";
+import Header from "./components/Header";
+
 
 export default function Cite() {
   return (
     <>
       <Background />
-      <section className="pt-36 mx-auto max-w-5xl px-6">
+      <section className="pt-24 mx-auto max-w-5xl px-6">
+        <Header />
         <UrlManager />
       </section>
     </>
---
+++ frontend/src/citation-section/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="mx-auto max-w-2xl text-center">
+      <h1 className="mb-6 text-[4.5rem] text-black">CiteAI</h1>
+    </div>
+  );
+};
+
+export default Header;
