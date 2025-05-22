91be405d7852e58f36a6df7e54e4312afd7cc0e7
Onur <onur.onel2230@gmail.com>
2025-05-21 20:04:27 -0400

    Update

--- frontend/src/citation-section/Cite.jsx
+++ frontend/src/citation-section/Cite.jsx
@@ -1,4 +1,5 @@
 import Background from "../components/Background";
+import Navbar from "../components/Navbar";
 import UrlManager from "./components/UrlManager";
 import Header from "./components/Header";
 
@@ -7,6 +8,7 @@ export default function Cite() {
   return (
     <>
       <Background />
+      <Navbar />
       <section className="pt-24 mx-auto max-w-5xl px-6">
         <Header />
         <UrlManager />
