f798f1f368d7485271c18a380524a0ee66981e7a
Onur <onur.onel2230@gmail.com>
2025-05-24 19:03:50 -0400

    Updates

--- frontend/src/components/Background.jsx
+++ frontend/src/components/Background.jsx
@@ -10,7 +10,7 @@ const Background = () => {
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
-          className="relative left-1/2 aspect-[1155/678] w-[64rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] via-[#c599ff] to-[#9089fc] opacity-25 sm:w-[120rem]"
+          className="relative left-1/2 aspect-[1155/678] w-[14rem] -translate-x-1/2 rotate-[10deg] bg-gradient-to-tr from-[#ff80b5] via-[#c599ff] to-[#9089fc] opacity-35 sm:w-[100rem]"
         />
       </div>
 
--- frontend/src/components/citation-section/Dropdown.jsx
+++ frontend/src/components/citation-section/Dropdown.jsx
@@ -1,5 +1,4 @@
 import {
-  Label,
   Listbox,
   ListboxButton,
   ListboxOption,
